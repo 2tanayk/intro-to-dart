0ab9b4a644f84ac294c1a54ad9fb20533f349f0c
Tanay Kamath <2tanaykamath@gmail.com>
2021-08-26 21:02:57 +0530

    function parameters in Dart

--- bin/intro_to_dart.dart
+++ bin/intro_to_dart.dart
@@ -14,10 +14,49 @@ import 'package:intro_to_dart/intro_to_dart.dart' as intro_to_dart;
 late String description; //lateinit
 final place = 'Mumbai'; // runtime constant
 const bar = 1000000; //compile time constant
+var _nobleGases = {
+  // Key:    Value
+  1: 'partridge',
+  2: 'turtledoves',
+  3: 'golden rings'
+};
+
 // Define a function.
 void printInteger(int aNumber) {
   print('The number is $aNumber.'); // Print to console.
 }
+
+//function
+//this also works on type omission due to type inference
+bool isNoble(int atomicNumber) {
+  return _nobleGases[atomicNumber] != null;
+}
+//For functions that contain just one expression, you can use a shorthand syntax
+// bool isNoble(int atomicNumber) => _nobleGases[atomicNumber] != null;
+
+//function with named parameters
+//named params are optional unless specified as required, and can be in any order while calling
+int threeSum({required int a, required int b, required int c}) {
+  return a + b + c;
+}
+
+//Wrapping a set of function parameters in [] marks them as optional positional parameters
+String say(String from, String msg, [String? device]) {
+  var result = '$from says $msg';
+  if (device != null) {
+    result = '$result with a $device';
+  }
+  return result;
+}
+
+//Your function can use = to define default values for both named and positional parameters. 
+//The default values must be compile-time constants. If no default value is provided, the default value is null.
+//Here’s an example of setting default values for named parameters
+/// Sets the [bold] and [hidden] flags ...
+// void enableFlags({bool bold = false, bool hidden = false}) {...}
+// bold will be true; hidden will be false.
+// enableFlags(bold: true);
+
 
 void main(List<String> arguments) {
   var number = 42; // Declare and initialize a variable, type inferred to int
@@ -101,5 +140,8 @@ void main(List<String> arguments) {
   print('using spread operator ${list3}');
   print(halogens);
   print(gifts);
+  print(isNoble(1));
+  print(threeSum(b: 1, a: 2, c: 10));
+  print(say('Tanay', 'DDSGS'));
   printInteger(10);
 }
